c652729f9fb51d4d4e446c1f08690dc4b42b3597
Travis <travismay@gmail.com>
2015-03-19 13:55:50 -0500

    Create clean.R

--- clean.R
+++ clean.R
@@ -1,1 +1,11 @@
 ##  clean.R
+clean <- function() {
+  twitter <- iconv(twitter, from = "latin1", to = "UTF-8", sub="")
+  twitter <- stri_replace_all_regex(twitter, "\u2019|`","'")
+  twitter <- stri_replace_all_regex(twitter, "\u201c|\u201d|u201f|``",'"')
+
+  # save the data to .RData files
+  save(blogs, file="blogs.RData")
+  save(news, file="news.RData")
+  save(twitter, file="twitter.RData")
+}

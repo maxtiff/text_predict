6b31fe1628720caa5881e28368940bfa1ec47a35
Travis <maxtiff@users.noreply.github.com>
2016-03-09 12:24:43 -0600

    Add data exploration code

--- main.R
+++ main.R
@@ -9,23 +9,35 @@ library(dplyr)
 library(magrittr)
 
 ## Source scripts
-required.scripts <- c('get.R','load.R','clean.R','sample.R')
+required.scripts  = c('get.R','load.R','clean.R','sample.R')
 sapply(required.scripts, source, .GlobalEnv)
 
 ## Get and extract data from source
-url <- 'https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip'
+url  = 'https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip'
 download(url)
 
 ## Load each data file into environment
-files <- c(list.files('./final/en_US'))
+files = c(list.files('./final/en_US'))
 
 for (i in files) {
-  name <- strsplit(i,"\\.")[[1]]
+  name = strsplit(i,"\\.")[[1]]
   assign(name[[2]],loader(i))
 }
+
+## 1st Quiz
+max(nchar(blogs))
+max(nchar(news))
+max(nchar(twitter))
+love_count <- sum(grepl("love", twitter))
+hate_count <- sum(grepl("hate", twitter))
+love_count / hate_count
+biostats <- grep("biostats", twitter)
+twitter[biostats]
+sum(grepl("A computer once beat me at chess, but it was no match for me at kickboxing", twitter))
 
 ## Clean data
 clean()
 
 ## Combine data into sample
 sampler()
+
